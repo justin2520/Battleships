2cb0763ab3ec2d3efd90f9b425d809ae093a6451
justin.weber2520 <justin.weber25@d155.org>
2024-03-22 11:26:32 -0500

    Started project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+
+        JPanel battleShips = new JPanel(new GridLayout(10, 10));
+
+        mainFrame.add(battleShips);
+
+        mainFrame.setVisible(true);
+    }
+}
